f592058982050178096f2a7e6e244a03e992228a
Victoria  Berro <victoriaberro@192.168.1.8>
2025-05-27 20:52:43 -0300

    etidades

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -6,7 +6,7 @@ public class Pelicula {
     private String idiomaOriginal;
     private int totalCalificaciones;
     private String[][] generoPelicula;
-    private Saga perteneceSaga;
+    private Coleccion perteneceSaga;
     private int presupuesto;
     private String link;
 
